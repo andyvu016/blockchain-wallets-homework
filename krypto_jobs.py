86b58c153220e2de81947a3432dcf1f15117277c
Andy Vu <78772249+andyvu016@users.noreply.github.com>
2023-09-27 00:06:06 -0400

    Completed Application

---
+++ krypto_jobs.py
@@ -0,0 +1,156 @@
+# Cryptocurrency Wallet
+# Imports
+import streamlit as st
+from dataclasses import dataclass
+from typing import Any, List
+from web3 import Web3
+
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+
+# From `crypto_wallet.py import the functions generate_account, get_balance,
+#  and send_transaction
+from crypto_wallet import generate_account, get_balance, send_transaction
+
+################################################################################
+# KryptoJobs2Go Candidate Information
+
+# Database of KryptoJobs2Go candidates including their name, digital address, rating and hourly cost per Ether.
+# A single Ether is currently valued at $1,500
+candidate_database = {
+    "Lane": [
+        "Lane",
+        "0x520A5f80e244e7d96AC0B3C1A0940E7133b2562B",
+        "4.3",
+        0.20,
+        "Images/lane.jpeg",
+    ],
+    "Ash": [
+        "Ash",
+        "0xDfacC911a8D878fb41fF9b573Ab7024aC278bfA1",
+        "5.0",
+        0.33,
+        "Images/ash.jpeg",
+    ],
+    "Jo": [
+        "Jo",
+        "0xea4454d6624d92b509a0E3c0E791d90a695fd32F",
+        "4.7",
+        0.19,
+        "Images/jo.jpeg",
+    ],
+    "Kendall": [
+        "Kendall",
+        "0x9d32C7cb8DD6c0ae3C3630048E4E5A52e874F30E",
+        "4.1",
+        0.16,
+        "Images/kendall.jpeg",
+    ],
+}
+
+# A list of the KryptoJobs2Go candidates first names
+people = ["Lane", "Ash", "Jo", "Kendall"]
+
+
+def get_people():
+    """Display the database of KryptoJobs2Go candidate information."""
+    db_list = list(candidate_database.values())
+
+    for number in range(len(people)):
+        st.image(db_list[number][4], width=200)
+        st.write("Name: ", db_list[number][0])
+        st.write("Ethereum Account Address: ", db_list[number][1])
+        st.write("KryptoJobs2Go Rating: ", db_list[number][2])
+        st.write("Hourly Rate per Ether: ", db_list[number][3], "eth")
+        st.text(" \n")
+
+
+################################################################################
+# Streamlit Code
+
+# Streamlit application headings
+st.markdown("# KryptoJobs2Go!")
+st.markdown("## Hire A Fintech Professional!")
+st.text(" \n")
+
+################################################################################
+# Streamlit Sidebar Code - Start
+
+st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
+
+#  Call the `generate_account` function and save it as the variable `account`
+account = generate_account()
+
+##########################################
+
+# Write the client's Ethereum account address to the sidebar
+st.sidebar.write(account.address)
+
+##########################################
+
+# Call `get_balance` function and pass it your account address
+# Write the returned ether balance to the sidebar
+st.sidebar.write(f"{get_balance(w3, account.address)} ETH")
+
+##########################################
+
+# Create a select box to chose a FinTech Hire candidate
+person = st.sidebar.selectbox("Select a Person", people)
+
+# Create a input field to record the number of hours the candidate worked
+hours = st.sidebar.number_input("Number of Hours")
+
+st.sidebar.markdown("## Candidate Name, Hourly Rate, and Ethereum Address")
+
+# Identify the FinTech Hire candidate
+candidate = candidate_database[person][0]
+
+# Write the KryptoJobs2Go candidate's name to the sidebar
+st.sidebar.write(candidate)
+
+# Identify the KryptoJobs2Go candidate's hourly rate
+hourly_rate = candidate_database[person][3]
+
+# Write the inTech Finder candidate's hourly rate to the sidebar
+st.sidebar.write(hourly_rate)
+
+# Identify the KryptoJobs2Go candidate's Ethereum Address
+candidate_address = candidate_database[person][1]
+
+# Write the inTech Finder candidate's Ethereum Address to the sidebar
+st.sidebar.write(candidate_address)
+
+# Write the KryptoJobs2Go candidate's name to the sidebar
+
+st.sidebar.markdown("## Total Wage in Ether")
+
+##########################################
+
+# Calculate total `wage` for the candidate by multiplying the candidate’s hourly
+# rate from the candidate database (`candidate_database[person][3]`) by the
+# value of the `hours` variable
+wage = hourly_rate*hours
+
+# Write the `wage` calculation to the Streamlit sidebar
+st.sidebar.write(f"{wage} ETH")
+
+##########################################
+
+if st.sidebar.button("Send Transaction"):
+
+    # Call the `send_transaction` function and pass it 3 parameters:
+    # Your `account`, the `candidate_address`, and the `wage` as parameters
+    # Save the returned transaction hash as a variable named `transaction_hash`
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
+
+    # Markdown for the transaction hash
+    st.sidebar.markdown("#### Validated Transaction Hash")
+
+    # Write the returned transaction hash to the screen
+    st.sidebar.write(transaction_hash)
+
+    # Celebrate your successful payment
+    st.balloons()
+
+# The function that starts the Streamlit application
+# Writes KryptoJobs2Go candidates to the Streamlit page
+get_people()
